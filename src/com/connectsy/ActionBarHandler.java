974890508f9d216c40fa8bd93363bd1383ada608
Patrick Stein <lylepstein@gmail.com>
2010-09-25 03:17:08 -0400

    Added logo link

--- src/com/connectsy/ActionBarHandler.java
+++ src/com/connectsy/ActionBarHandler.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 import com.connectsy.events.EventNew;
 
@@ -12,11 +13,16 @@ public class ActionBarHandler implements OnClickListener{
 	
 	public ActionBarHandler(Activity a){
 		activity = a;
+		
+		ImageView logo = (ImageView)a.findViewById(R.id.ab_logo);
+		logo.setOnClickListener(this);
 	}
 	
 	public void onClick(View abAction) {
 		if (abAction.getId() == R.id.ab_new_event){
     		activity.startActivity(new Intent(activity, EventNew.class));
+    	} else if (abAction.getId() == R.id.ab_logo) {
+    		activity.startActivity(new Intent(activity, Dashboard.class));
     	}
 	}
 
